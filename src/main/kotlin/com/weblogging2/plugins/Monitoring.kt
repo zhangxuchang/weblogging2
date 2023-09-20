fab14d6f2de24d9be9a3d0ace52fb668fa5320a1
zhangxuxuchang <44724385@qq.com>
2023-09-20 19:05:09 +0800

    test MDC

--- src/main/kotlin/com/weblogging2/plugins/Monitoring.kt
+++ src/main/kotlin/com/weblogging2/plugins/Monitoring.kt
@@ -15,12 +15,12 @@ fun Application.configureMonitoring() {
         format { call ->
             val status = call.response.status()
             val httpMethod = call.request.httpMethod.value
-            val userAgent = call.request.headers["Host"]
+            val userAgent = call.request.headers["User-Agent"]
             val reqKey = MDC.get("reqKey")
             "Status: $status, req-key:$reqKey HTTP method: $httpMethod, User agent: $userAgent"
         }
         mdc("reqKey") { call ->
-            call.request.headers["Sec-Ch-Ua-Platform"]
+            call.request.headers["Host"]
         }
     }
 }
