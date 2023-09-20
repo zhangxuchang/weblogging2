8697d7a77123255d527c64175688ac44fa649e05
zhangxuxuchang <44724385@qq.com>
2023-09-20 17:53:57 +0800

    project init

---
+++ src/main/kotlin/com/weblogging2/Application.kt
@@ -0,0 +1,13 @@
+package com.weblogging2
+
+import com.weblogging2.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureRouting()
+}
